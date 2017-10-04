16fcb5c564d9d1e8693e4e771a00fef6d9d07583
Ryan <promauriya@gmail.com>
2017-10-04 01:04:20 -0400

    Updates

--- confirmmessage.php
+++ confirmmessage.php
@@ -23,27 +23,20 @@ function confirm_message() {
     $position = 23;
 
     add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-	add_submenu_page( $menu_slug,'System Test', 'System Test', 'manage_options', 'sysryn_tstjs', 'sysryn_tstjs' );
 }
 /* Determine The Time Location START */
 date_default_timezone_set("America/New_York");
 /* Determine The Time Location END */
 function confirm_message_sent() {
         if (isset($_POST['email_address'])) {
-	        echo $_POST['email_address'];
+	        if (isset($_POST['email_subject'])) {
+	           if (isset($_POST['email_message'])) {
+	               if (isset($_POST['email_valid'])) {
+                        message_email($_POST['email_address'],$_POST['email_subject'],$_POST['email_message'],$_POST['email_valid']);
+	               }
+	           }
+	        }
 	    }
-        if (isset($_POST['email_subject'])) {
-	        echo $_POST['email_subject'];
-	    }
-	    if (isset($_POST['email_message'])) {
-	        echo $_POST['email_message'];
-	    }
-        if (isset($_POST['email_valid'])) {
-	        echo $_POST['email_valid'];
-	    }
-        $obj = new myemail();
-        $reply = $obj->composeemail($_POST['email_address'],$_POST['email_subject'],$_POST['email_message'],$_POST['email_valid']);
-        var_dump($reply); 
     
     include 'setting/sent.php';
 }
@@ -52,17 +45,27 @@ function confirm_message_sent() {
 // add_action( 'wp_footer', 'export_ryanEXLS');
 /* Run JS script in Different Position in HOOK END */
 /* Notice START */
-add_action( 'admin_notices', 'email_sent_success' );
-function email_sent_success() {
-     if (isset($_POST['email_valid'])) {
+//add_action( 'admin_notices', 'email_sent_success' );
+function email_sent_success($messg) {
     ?>
     <div class="notice notice-success is-dismissible">
-        <p>The Email was sent!</p>
+        <p><?php echo $messg; ?></p>
     </div>
     <?php
-     }
 }
 /* Notice END */
+/* Sent Email Message Message START */
+function message_email($to,$sub,$messg,$valie) {
+    $objt = new myemail();
+    $reply = $objt->composeemail($to,$sub,$messg,$valie);
+    if($reply == 1) {
+         email_sent_success('The Email was sent!');
+    } else {
+         email_sent_success('The Email not sent!');
+    }
+    unset($objt);
+}
+/* Sent Email Message Message END */
 /* Send Simple Email START */
 class myemail {
     public $headers = 'From: Ryan Mahato <promauriya@gmail.com>';
@@ -71,7 +74,7 @@ class myemail {
         return $reply;
     }
     public function composeemail($to, $subject, $message, $timelimit){
-        $reply = self::mailsent(($to, $subject, $message);
+        $reply = self::mailsent($to, $subject, $message);
         return $reply;
     }
 }
